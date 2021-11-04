d82e051319bae3fb3163d2bd3fea5f03b5c4e928
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-11-04 03:27:24 +0300

    Update founding event.sol

--- Fantom/founding event.sol
+++ Fantom/founding event.sol
@@ -4,6 +4,8 @@ interface I{
 	function getPair(address t, address t1) external view returns(address pair);
 	function createPair(address t, address t1) external returns(address pair);
 	function genesis(uint Ftm,address pair,uint gen) external;
+	function genesis(uint b) external;
+	function genesis(uint b, address p) external;
 	function transfer(address to, uint value) external returns(bool);
 	function balanceOf(address) external view returns(uint);
 	function addLiquidityETH(address token,uint amountTokenDesired,uint amountTokenMin,uint amountETHMin,address to,uint deadline)external payable returns(uint amountToken,uint amountETH,uint liquidity);
@@ -60,8 +62,10 @@ contract FoundingEvent {
 		address factory = 0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3;
 		address router = 0xF491e7B69E4244ad4002BC14e878a34207E38c29;
 		address tknFTMLP = I(factory).getPair(_letToken,WFTM);
+        address letToken=0x7DA2331C522D4EDFAf545d2F5eF61406D9d637A9;
+		address treasury=0x6B51c705d1E78DF8f92317130a0FC1DbbF780a5A;
 		if (tknFTMLP == address(0)) {
-			tknFTMLP=I(factory).createPair(_letToken, WFTM);
+			tknFTMLP=I(factory).createPair(letToken, WFTM);
 		}
 		uint balance = address(this).balance;
 		uint amount;
@@ -69,11 +73,11 @@ contract FoundingEvent {
 			amount = 1e23*balance/hardcap;
 		}
 		//I(_letToken).approve(address(router), 1e23);//careful, if token contract does not have hardcoded allowance for the router you need this line
-		I(router).addLiquidityETH{value: address(this).balance}(_letToken,amount,0,0,staking,2**256-1);
+		I(router).addLiquidityETH{value: address(this).balance}(letToken,amount,0,0,staking,2**256-1);
 		I(staking).genesis(balance, tknFTMLP,block.number);
-		I(_letToken).transfer(treasury,I(_letToken).balanceOf(address(this)));// burn excess to treasury, in case if hardcap is not reached by the end date is
-		I(_letToken).genesis(block.number);
-		I(_treasury).genesis(block.number);
+		I(letToken).transfer(treasury,I(letToken).balanceOf(address(this)));// burn excess to treasury, in case if hardcap is not reached by the end date is
+		I(letToken).genesis(block.number);
+		I(treasury).genesis(block.number);
 		delete _lgeOngoing;
 	}
 
@@ -100,7 +104,7 @@ contract FoundingEvent {
     	I(_letToken).transfer(_deployer, 1e23);
     	delete _lgeOngoing;
     }
-
+//in case of migration
     function addFounderManually(address a) external payable{
     	require(msg.sender == _deployer);
     	uint amount = msg.value;
@@ -108,5 +112,6 @@ contract FoundingEvent {
     	amount -= deployerShare;
     	_deployer.transfer(deployerShare);
     	deposits[a]+=amount;
+    	require(deposits[a]<=5e20);
     }
 }
